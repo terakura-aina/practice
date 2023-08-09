27b826a17947391b0673fadf91a060e5aa23c2aa
terakura.aina <aicaierwei@gmail.com>
2023-08-09 09:51:32 +0900

    create form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,103 +4,67 @@ import styles from './page.module.css'
 
 export default async function Home() {
   const res: any = await data()
-  console.log("test")
   console.log(res)
+  {res.map((fruits: any) => {
+    console.log(fruits.name)
+  })}
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <ul>
-          {res.map((fruits: any) => {
-            return (
-              <li>{fruits.name}</li>
-            )
-          })}
-        </ul>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className={styles.signup}>
+          <div className={styles.signupTitle} />
+          <div className={styles.signupDescription}>
+            登録して友達の写真や動画をチェックしよう
+          </div>
+          <button className={styles.signupFacebook}>
+            <span className={styles.sugnupFacebookLogo} />
+            Facebookでログイン
+          </button>
+          <div className={styles.signupOr}>または</div>
+          <form className={styles.signupForm}>
+            <div className={styles.signupInputUnity}>
+              <label className={styles.signupLabel}>
+                <input className={styles.signupInput} type='text' placeholder=' ' />
+                <span className={styles.signupInputDescription}>携帯電話番号またはメールアドレス</span>
+              </label>
+            </div>
+            <div className={styles.signupInputUnity}>
+              <label className={styles.signupLabel}>
+                <input className={styles.signupInput} type='text' placeholder=' ' />
+                <span className={styles.signupInputDescription}>フルネーム</span>
+              </label>
+            </div>
+            <div className={styles.signupInputUnity}>
+              <label className={styles.signupLabel}>
+                <input className={styles.signupInput} type='text' placeholder=' ' />
+                <span className={styles.signupInputDescription}>ユーザーネーム</span>
+              </label>
+            </div>
+            <div className={styles.signupInputUnity}>
+              <label className={styles.signupLabel}>
+                <input className={styles.signupInput} type='passward' placeholder=' ' />
+                <span className={styles.signupInputDescription}>パスワード</span>
+              </label>
+            </div>
+            <div className={styles.signupNotes}>
+              <span>
+                サービスの利用者があなたの連絡先情報をInstagramにアップロードしている場合があります。
+              </span>
+              <a className={styles.signupNotesReadMore} href='https://www.facebook.com/help/instagram/261704639352628'>
+                詳しくはこちら
+              </a>
+            </div>
+            <div className={styles.signupTermsOfUse}>
+              <span>登録することで、</span>
+              <a href='https://help.instagram.com/581066165581870/?locale=ja_JP'>Facebookの利用規約</a>
+              <span>、</span>
+              <a href='https://www.facebook.com/privacy/policy'>プライバシーポリシー</a>
+              <span>、</span>
+              <a href='https://help.instagram.com/1896641480634370/'>Cookieポリシー</a>
+              <span>に同意するものとします。</span>
+            </div>
+            <input type='submit' value='登録する' className={styles.signupSubmit} disabled />
+          </form>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
