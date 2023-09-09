025f4bc9b43e5879527b4ad2ecc5d332fd9841d0
terakura.aina <aicaierwei@gmail.com>
2023-09-09 11:10:32 +0900

    deled unwanted codes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,8 @@
-import Image from "next/image"
-import data from "./data"
 import styles from "./page.module.css"
 import { Button } from "./components/Button"
 import { RegisterForm } from "./components/RegisterForm"
 
 export default async function Home() {
-  const res: any = await data()
-  console.log(res)
-  {
-    res.map((fruits: any) => {
-      console.log(fruits.name)
-    })
-  }
   return (
     <main className={styles.main}>
       <div className={styles.signup}>
